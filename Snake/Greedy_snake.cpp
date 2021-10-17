9626c805457711b1962cdc8bfcd4b70f5dc0836a
虫 虫 <15971267587@163.com>
2021-10-17 10:45:18 +0800

    画蛇

--- Snake/Greedy_snake.cpp
+++ Snake/Greedy_snake.cpp
@@ -1,21 +1,69 @@
 #include<stdio.h>
 #include<graphics.h>
+#include<Windows.h>
+#include<time.h>
+#include<conio.h>  //控制台输入输出头文件
+#include<stdlib.h>
 
-using namespace std;
+#define SNAKE_NUM 400000  //蛇的最大节数
 
-int main()
+//蛇的结构
+struct Snake
 {
-	//初始化 图形窗口
+	int size;  //蛇的节数
+	int dir;  //方向
+	int speed;  //速度
+	POINT coor[SNAKE_NUM];  //坐标
+}snake;
+
+//数据初始化
+void GameInit()
+{
+	//初始化图形窗口
 	initgraph(800, 600);
-	//设置背景
+	// 初始化蛇
+	snake.size = 3;
+	snake.speed = 10;
+	snake.dir;
+	for (int i = snake.size - 1; i >= 0; i--)
+	{
+		snake.coor[i].x = 10 * i + 400;
+		snake.coor[i].y = 300;
+	}
+}
+
+//绘图
+void GameDraw()
+{
+	//绘制背景
 	IMAGE img;
 	int wide = 800, height = 600;
 	loadimage(&img, L"C:\\Users\\15971\\source\\repos\\Greedy Snake\\Snake\\bk.jpg", wide, height);
 	putimage(0, 0, &img);
+	//绘制蛇
+	setfillcolor(RGB(126, 239, 123));
+	for (int i = 0; i < snake.size; i++)
+	{
+		solidcircle(snake.coor[i].x, snake.coor[i].y, 5);
+	}
+}
 
+//移动蛇
+void snakeMove()
+{
 
+}
+
+int main()
+{
+
+	GameInit();
+	GameDraw();
+
+	
 	while (1)
 	{
 
 	}
+	return 0;
 }
